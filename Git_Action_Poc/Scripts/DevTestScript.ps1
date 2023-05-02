4d560b3fc146126399a7cf35b930dc5481c3b587
jimmy <jimmy@gmail.com>
2023-05-02 15:22:56 +0530

    website checking

--- Git_Action_Poc/Scripts/DevTestScript.ps1
+++ Git_Action_Poc/Scripts/DevTestScript.ps1
@@ -1,4 +1,4 @@
-﻿$DEV_WEBSITE_NAME="DevGit_New_Test2"
+﻿$DEV_WEBSITE_NAME="dev_web_Git"
 $dir = "C:\inetpub\wwwroot\$DEV_WEBSITE_NAME"
 if(!(Test-Path -Path $dir )){
     New-Item -ItemType directory -Path $dir
